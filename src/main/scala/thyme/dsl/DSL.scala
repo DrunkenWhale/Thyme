5d21c674b54151d9bc9407d255c3ee44b76fd5e5
pigeon_gugugu <892197240@qq.com>
2022-04-10 19:55:34 +0800

    feat: DSL class to export function in one file

---
+++ src/main/scala/thyme/dsl/DSL.scala
@@ -0,0 +1,34 @@
+package thyme.dsl
+
+object DSL {
+
+  export thyme.dsl.Path.path
+  export thyme.dsl.Path.~
+  export thyme.dsl.Middleware.interceptor
+  export thyme.dsl.ContextParam.parameter
+  export thyme.dsl.ContextParam.parameters
+  export thyme.dsl.ContextParam.as
+  export thyme.dsl.ContextParam.form
+  export thyme.dsl.ContextParam.forms
+  export thyme.dsl.ContextParam.attribute
+  export thyme.dsl.ContextParam.attributes
+  export thyme.dsl.ContextParam.header
+  export thyme.dsl.ContextParam.headers
+  export thyme.dsl.ContextParam.json
+  export thyme.dsl.ContextParam.route
+  export thyme.dsl.ContextParam.routes
+  export thyme.dsl.rest.Put.put
+  export thyme.dsl.rest.Get.get
+  export thyme.dsl.rest.Post.post
+  export thyme.dsl.rest.Delete.delete
+  export thyme.dsl.rest.Route.route
+
+  export rosemary.parser.Parser.parse
+  export rosemary.stringify.Stringify.stringify
+  export rosemary.stringify.Stringify.toJson
+  export rosemary.stringify.Generate.arr
+  export rosemary.stringify.Generate.json
+  export rosemary.stringify.Generate.obj
+  export rosemary.stringify.conv.ValueToJsonType.given
+
+}
